3838c2371afa5c0e81f04ec2619a70da7af35e29
Sachini Tharanga <sachinitharanga2020al@gmail.com>
2024-04-22 20:17:09 +0530

    added calculator file

--- src/calculater/Calculater.java
+++ src/calculater/Calculater.java
@@ -4,17 +4,69 @@
  */
 package calculater;
 
-/**
- *
- * @author S
- */
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class Calculater {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Scanner sc = new Scanner (System.in);
+        double num1, num2;
+        double result = 0;
+        int choice;
+        
+        System.out.println("Simple Calculator");
+        System.out.println("Choose an operation");
+        System.out.println("1: Addition");
+        System.out.println("2: Subtraction");
+        System.out.println("3: Multiplication");
+        System.out.println("4: Division");
+        System.out.println("5: Exit");
+        
+        do {
+            System.out.println("Enter choice (1/2/3/4/5):");
+        choice = sc.nextInt();
+        
+        if (choice == 5){
+            System.out.println("Existing Program.");
+            break;
+        }
+        System.out.println("Enter first number: ");
+        num1 =sc.nextDouble();
+        
+        System.out.println("Enter second number: ");
+        num2 = sc.nextDouble();
+        
+        switch(choice){
+            case 1 :
+                result = num1 + num2;
+                System.out.printf("Result: %.2f\n", result);
+                break;
+            case 2 :
+                result = num1 - num2;
+                System.out.printf("Result: %.2f\n", result);
+                break;
+            case 3 :
+                result = num1 * num2;
+                System.out.printf("Result: %.2f\n", result);
+                break;
+            case 4 :
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.printf("Result: %.2f\n", result);
+                    } else {
+                    System.out.println("Division by zero is not allowed.");
+                    }
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid oprtaion.");
+                
+        }
+        } while (choice != 5);
+        
+        sc.close();
+        }
     
 }
